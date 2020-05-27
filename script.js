455fc8b16c5bcd62cc9d8ebbda4678edb8d3cde6
ana komase <elizakomase@gmail.com>
2020-05-27 14:16:08 -0300

    formulario jquery validação

---
+++ script.js
@@ -0,0 +1,14 @@
+//validaçao formulario
+$("#formulario").validate();
+//mascara para telefone
+$("#telefone").mask('(00)00000-0000');
+//mascara para cpf
+$("#CPF").mask('000.000.000-00');
+//mascara para data de nascimento
+$("#datanascimento").mask('00/00/0000');
+//mascara para cep
+$("#CEP").mask('00000-000');
+//mascara email
+$("#email").mask('AAAA@AAA.AAA')
+//mascara estado
+$("#estado").mask('AA')
